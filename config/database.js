1b096f6d77676b5d15bc3e4f3ddf0707954a83d4
Viorel Moldovan <viorelmoldovan@gmail.com>
2023-08-24 18:14:10 +0300

    2nd render

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: "./.env"});
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGODB_URI;
@@ -9,5 +9,6 @@ mongoose.connect(mongoURI, {
 }).then(() => {
     console.log('>>> Connected to MongoDB');
 }).catch((error) => {
+    console.log('>>>', process.env.MONGODB_URI);
     console.error('Error connecting to MongoDB:', error);
 });
